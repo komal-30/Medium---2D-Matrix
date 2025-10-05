98c1dceb37d141a0971a696f9014e1ebaea9cfdf
Komal Rane <rane.komalsandy@gmail.com>
2025-10-05 16:22:00 +0100

    Create 7-Spiral_Traversal.py

---
+++ 7-Spiral_Traversal.py
@@ -0,0 +1,45 @@
+#Brute Force
+
+
+
+
+#Ideal
+def spiral_matrix(mat):
+    ans=[]
+    n = len(mat)  #Row
+    m = len(mat[0]) #Column
+
+    srow=0
+    erow=n-1
+    scol=0
+    ecol=m-1
+
+    while(srow<=erow and scol <= ecol):
+        #Top
+        for i in range(scol,ecol+1):
+            ans.append(mat[srow][i])
+        srow+=1
+        
+        #Right
+        for j in range(srow,erow+1):
+            ans.append(mat[j][ecol])
+        ecol-=1
+
+        #Bottom
+        if srow <= erow:
+            for k in range(ecol,scol-1,-1):
+                ans.append(mat[erow][k])
+            erow-=1
+
+        #Left
+        if scol<=ecol:
+            for l in range(erow,srow-1,-1):
+                ans.append(mat[l][scol])
+            scol+=1
+
+    return ans
+
+
+
+
+#Optimal
